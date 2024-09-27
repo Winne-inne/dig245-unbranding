f63b73c8c565016e437ca5dbcd032ea849ba760b
Hello2006_debug <memugengano@davidson.edu>
2024-09-26 20:40:34 -0400

    javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,17 @@
-
 /* javascript */
+let entire = document.querySelector(".entire");
+let page1 = document.querySelector(".page1");
+let page2 = document.querySelector(".page2");
+let entire = 1;
+
+page1.addEventListener("mouseover",function(){
+    if (entire==1){
+        visibility=2;
+        page1.style.display="none"
+        page2.style.display="block"
+    }else{
+        visibility=1
+
+    }
+    this.style.display = "none"
+})
